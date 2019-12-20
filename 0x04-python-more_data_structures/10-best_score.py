bfc8f3286b33a461faf4c1d903eea699d0964c31
steven-cruz <stevencruz.300008@gmail.com>
2019-12-20 04:13:31 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
-        return None
+        return
     n = []
     for new in a_dictionary:
         n.append(new)
