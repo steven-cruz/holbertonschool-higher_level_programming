a04ccf48b89e3f06a3c56ae5e50cc6c521f199cf
steven-cruz <stevencruz.300008@gmail.com>
2019-12-20 04:16:28 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return max(n)
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value == max(a_dictionary.values()):
+                return key
+    return None
