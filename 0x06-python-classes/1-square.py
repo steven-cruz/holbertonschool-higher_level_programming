c8727a793e2cfe08e65d3d195fbaee529dfc74ef
steven-cruz <stevencruz.300008@gmail.com>
2020-01-10 02:28:45 +0000

    private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' private instations attribute of size'''
+
+
+class Square:
+    '''create Square type'''
+    def __init__(self, size):
+        '''initializes Square with size'''
+        self.__size = size
