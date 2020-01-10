28e6445af9cfb2c5f48b87b6bc34d80e90f02f0c
steven-cruz <stevencruz.300008@gmail.com>
2020-01-10 04:10:24 +0000

    defines the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         ''' defines the area of a squere '''
         return self.__size * self.__size
