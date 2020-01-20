814f3ed94a50480cde108792f784ad71ef7841a1
steven-cruz <stevencruz.300008@gmail.com>
2020-01-20 22:54:12 +0000

    inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,5 @@
 class MyList(list):
     ''' MyList class inherits from the parent list class'''
 
-
     def print_sorted(self):  # public instance method
         print(sorted(self))  # print the list, but sorted(ascending sort)
