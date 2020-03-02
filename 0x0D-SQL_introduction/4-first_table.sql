f663f08b58dd8589e2144741d5e4b2c792e8a2f5
steven-cruz <stevencruz.300008@gmail.com>
2020-03-02 19:49:21 +0000

    create the first table of a database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates a table called first_table in the current database.
+-- The database name will be passed as an argument of the mysql command.
+-- If the table first_table already exists, your script should not fail.
+CREATE TABLE first_table (id INT, name VARCHAR(256));
