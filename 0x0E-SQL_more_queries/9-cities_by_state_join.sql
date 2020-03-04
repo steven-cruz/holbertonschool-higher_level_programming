f0e139c23e225a4c54b58da39e7592f593861f76
steven-cruz <stevencruz.300008@gmail.com>
2020-03-04 03:53:33 +0000

    lists all cities contained in the database

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM states, cities WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
