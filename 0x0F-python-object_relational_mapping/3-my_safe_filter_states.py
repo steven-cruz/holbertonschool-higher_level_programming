46a46b8509f60e57f142eb090e2f910449759988
steven-cruz <stevencruz.300008@gmail.com>
2020-03-29 17:52:33 +0000

    List Items from MySQL

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# List Items from MySQL
+
+import MySQLdb
+
+
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY\
+    id ASC", (argv[4],))
+    for rows in c.fetchall():
+        print(rows)
+
+    c.close()
+    db.close()
