b3947148a63a2078a3b64e3d910232188b757f52
steven-cruz <stevencruz.300008@gmail.com>
2020-03-30 00:40:42 +0000

    deletes all state objects with the name containing the lleter a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,4 @@
-#!/usr/bin7python3
+#!/usr/bin/python3
 # Deletes all states object with a name containing the letter
 # a from the database.
 
@@ -11,13 +11,13 @@ from model_state import Base, State
 
 
 if __name__ == '__main__':
-    engine = create_engine('mys+mysqldb://{}:{}@localhost/{}'.format
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    item = session.query(State).filter(State.name.liked('%a%'))
+    item = session.query(State).filter(State.name.like('%a%'))
     for it in item:
         session.delete(it)
     session.commit()
