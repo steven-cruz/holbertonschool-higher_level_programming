d6e5edc1feb28c44aea31904c63c9e0dc1c67e49
steven-cruz <stevencruz.300008@gmail.com>
2020-04-17 01:58:47 +0000

    sends request to the URL

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # python script that takes in a URL, sends a request to the URL
 # and display the body of the responde (decoded in utf-8).
+
 import urllib.request as ur
 import urllib.error as ure
 from sys import argv
@@ -13,5 +14,5 @@ if __name__ == '__main__':
     try:
         with ur.urlopen(req) as response:
             print(str(response.read(), 'utf-8'))
-    except ure.HTTPerror as err:
+    except ure.HTTPError as err:
         print("Error code: {}".format(err.getcode()))
