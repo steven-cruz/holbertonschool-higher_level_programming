2e46907d3f0191b8e127711fcf306573eec87a39
steven-cruz <stevencruz.300008@gmail.com>
2020-05-05 21:37:24 +0000

    prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that prints 3 Lines.
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'Javascript is amazing';
+
+// print all outputs with console.log()
+console.log(line1);
+console.log(line2);
+console.log(line3);
