b4ebd70a956c8e1d3ffe1da7c68b528762cc4749
steven-cruz <stevencruz.300008@gmail.com>
2020-05-06 00:50:26 +0000

    print arguments or undefined

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+Script that prints two arguments passed to it, in the following format:
+argument_1 is argument_2
+*/
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
