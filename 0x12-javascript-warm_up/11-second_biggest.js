6cd757552a68a752c3a5040ccc4531550329ab38
steven-cruz <stevencruz.300008@gmail.com>
2020-05-06 04:31:56 +0000

    searches the second biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that searches the second biggest integer in the list of arguments.
+let list = [];
+let count = 2;
+const arg = process.argv.length;
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  for (count = 2; count < arg; count++) {
+    list.push(Number(process.argv[count]));
+  }
+  console.log(list.sort()[list.length - 2]);
+}
