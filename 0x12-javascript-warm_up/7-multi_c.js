02455fb0d35962b2ed7cdabfd729952d8ed18d41
steven-cruz <stevencruz.300008@gmail.com>
2020-05-06 02:49:09 +0000

    prints x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that prints x times C is fun.
-let x = process.argv[2];
+const x = process.argv[2];
 let count = 0;
 
 if (isNaN(process.argv[2])) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
 
 for (count = 0; count < x; count++) {
