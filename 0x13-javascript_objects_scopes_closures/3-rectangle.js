15e2d9f884691f01e5fe04372551282fd323e689
steven-cruz <stevencruz.300008@gmail.com>
2020-05-12 03:30:01 +0000

    create an instance method called

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,12 +5,11 @@ class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.heigth = h;
+      this.height = h;
     }
   }
-
   print () {
-    for (let count = 0; count < this.heigth; count++) {
+    for (let count = 0; count < this.height; count++) {
       console.log('X'.repeat(this.width));
     }
   }
