fc971ebda6df3db98a993aaf9f29799190e5118b
steven-cruz <stevencruz.300008@gmail.com>
2020-05-12 05:01:54 +0000

    returns the number of ocurrences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list.
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
