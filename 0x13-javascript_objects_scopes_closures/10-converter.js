b016bf11b8d209c28116b30dc343e6eee218698c
steven-cruz <stevencruz.300008@gmail.com>
2020-05-12 05:26:36 +0000

    convert a number from base 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// convets a number from base 10 to another base passed as argument.
+exports.converter = function (base) {
+  function convert (number) {
+    return number.toString(base);
+  }
+  return convert;
+}
