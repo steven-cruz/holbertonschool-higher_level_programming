e0e9bb9af095aea3d257e8ffa0a8c5b6d2345aff
steven-cruz <stevencruz.300008@gmail.com>
2020-05-12 02:54:17 +0000

    Create an instance method called print()

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// create an instance method calle print()
+
+class Rectangle{
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.heigth = h;
+    }
+  }
+  print() {
+    for (let count = 0; count < this.heigth; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
