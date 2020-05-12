05a11b6e5bd9bdf4a413bb0ffa13467c39d909c9
steven-cruz <stevencruz.300008@gmail.com>
2020-05-12 05:10:44 +0000

    return the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Function that return the reversed version of a list.
+exports.esrever = function (list) {
+  let new_list = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    new_list.push(list[i]);
+  }
+  return new_list;
+}
