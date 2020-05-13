57e2249f5eae6c579c3cdc162390eb19c48649dc
steven-cruz <stevencruz.300008@gmail.com>
2020-05-13 02:09:33 +0000

    write a strign to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//Script That Write a string to a file.
+// Script That Write a string to a file.
 const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(file, content, 'utf8', function write (err) {
   if (err) {
-    console.log(erro);
+    console.log(err);
   }
 });
