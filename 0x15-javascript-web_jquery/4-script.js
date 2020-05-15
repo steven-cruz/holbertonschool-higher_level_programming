82bf52bd5f0abf402f8e5809bb3f5aadb87d1d25
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 21:24:42 +0000

    DIV#toggle_header is added

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -4,6 +4,6 @@
   - The HEADER tag must always have one class: red or green,
     never both in the same time, never empty.
 */
-$('#toggle_header').click(function () {
-  $('header').toggleClass('red green');
+$('DIV#toggle_header').click(function () {
+  $('HEADER').toggleClass('red green');
 });
