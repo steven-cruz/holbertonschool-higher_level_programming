92f2c8fd20f1dd63af4953339fa272ed83965b56
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 21:26:20 +0000

    DIV#add_item is added

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -2,7 +2,7 @@
   Write a Javascript script that adds a LI element to a list when the user
   clicks on the tag DIV#add_item
 */
-$('#add_item').click(function () {
+$('DIV#add_item').click(function () {
   const $ul = $('UL.my_list');
   $ul.append('<li>Item</li>');
 });
