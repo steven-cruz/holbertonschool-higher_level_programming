f3c47923bce2faf0217ffd398220f77a3afc2e36
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 22:10:47 +0000

    fix

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -2,6 +2,6 @@
   Javascript that adds the class red to the HTML tag HEADER to red (#FF0000)
   when the user clicks on the tag DIV#red_header.
 */
-('DIV#red_header').click(function () {
+$('DIV#red_header').click(function () {
   $('HEADER').addClass('red');
 });
