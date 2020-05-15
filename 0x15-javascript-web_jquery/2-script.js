33b1399cb1a2a2d209b2bbdc6be8e7f47b3b5b34
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 21:21:26 +0000

    DIV # red_header is added

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -2,6 +2,6 @@
   JavaScript that updates the tect color of the HTML tag HEADER to red
   (#FF0000) when the user clicks on the tag DIV#red_herader
 */
-$('#red_header').click(function () {
-  $('header').css('color', '#FF0000');
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
 });
