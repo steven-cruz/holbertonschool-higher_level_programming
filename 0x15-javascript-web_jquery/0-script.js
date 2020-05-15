068019a9a8c0dbc0e29d94003a7b2ddae4a4ece3
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 21:15:40 +0000

    update header labels to uppercase letters

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,2 @@
 // Updates the text color of the HTML tag header to red (#FF0000).
-document.querySelector('header').style.color = '#FF0000';
+document.querySelector('HEADER').style.color = '#FF0000';
