a3f21f565f0b0577c3c39583bad998922637ade8
steven-cruz <stevencruz.300008@gmail.com>
2020-05-15 04:34:36 +0000

    script that fetches and replaces the name

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+/*
+  Javascript that fetches and replaces the name of this URL:
+  https://swapi-api.hbtn.io/api/people/5/?format=json
+*/
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+$.get(url, function (data) {
+  $('#character').text(data.name);
+});
